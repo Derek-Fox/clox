6a8218e9faa7acc37d066dba6e31b51d79692c8f
Derek Fox <derekefox@gmail.com>
2024-12-11 06:29:03 -0500

    Bug that I cannot track down! Fun

--- src/main.c
+++ src/main.c
@@ -2,12 +2,21 @@
 #include "chunk.h"
 #include "debug.h"
 
-int main(int argc, const char *argv[]) { 
+#include <stdio.h>
+
+int main() { 
+  printf("%ld\n", sizeof(int));
   Chunk chunk;
   initChunk(&chunk);
-  writeChunk(&chunk, OP_RETURN);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
+
   return 0;  
 }
