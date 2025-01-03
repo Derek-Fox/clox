c89a1699725401b580110d93bfec2d1cfc379893
Derek Fox <derekefox@gmail.com>
2025-01-03 17:17:50 -0500

    Finish scanner

--- src/scanner.c
+++ src/scanner.c
@@ -45,6 +45,133 @@ static bool match(char expected) {
   return true;
 }
 
+static char peek() { return *scanner.current; }
+
+static char peekNext() {
+  if (isAtEnd()) return '\0';
+  return scanner.current[1];
+}
+
+static void skipWhiteSpace() {
+  for (;;) {
+    char c = peek();
+    switch (c) {
+      case '\t':
+      case ' ':
+      case '\r':
+        advance();
+        break;
+      case '\n':
+        scanner.line++;
+        advance();
+        break;
+      case '/':  // comment may as well be whitespace
+        if (peekNext() == '/') {
+        } else {
+          return;
+        }
+        break;
+      default:
+        return;
+    }
+  }
+}
+
+static Token string() {
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n') scanner.line++;
+    advance();
+  }
+
+  if (isAtEnd()) return errorToken("Unterminated string literal.");
+
+  advance();  // consume closing quote
+  return makeToken(TOKEN_STRING);
+}
+
+static bool isDigit(char c) { return '0' <= c && c <= '9'; }
+
+static Token number() {
+  while (isDigit(peek())) advance();
+
+  // look for fraction
+  if (peek() == '.' && isDigit(peekNext())) {
+    advance();  // consume '.'
+
+    while (isDigit(peek())) advance();
+  }
+
+  return makeToken(TOKEN_NUMBER);
+}
+
+static bool isAlpha(char c) {
+  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || (c == '_');
+}
+
+static TokenType checkKeyword(int start, int length, const char* rest,
+                              TokenType type) {
+  if ((scanner.current - scanner.start == start + length) &&
+      (memcmp(scanner.start + start, rest, length) == 0))
+    return type;
+  return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+  switch (scanner.start[0]) {
+    case 'a':
+      return checkKeyword(1, 2, "nd", TOKEN_AND);
+    case 'c':
+      return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+    case 'e':
+      return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+    case 'f':
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'a':
+            return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+          case 'o':
+            return checkKeyword(2, 1, "r", TOKEN_FOR);
+          case 'u':
+            return checkKeyword(2, 1, "n", TOKEN_FUN);
+        }
+      }
+      break;
+    case 'i':
+      return checkKeyword(1, 1, "f", TOKEN_IF);
+    case 'n':
+      return checkKeyword(1, 2, "il", TOKEN_NIL);
+    case 'o':
+      return checkKeyword(1, 1, "r", TOKEN_OR);
+    case 'p':
+      return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+    case 'r':
+      return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+    case 's':
+      return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+    case 't':
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'h':
+            return checkKeyword(2, 2, "is", TOKEN_THIS);
+          case 'r':
+            return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+        }
+      }
+      break;
+    case 'v':
+      return checkKeyword(1, 2, "ar", TOKEN_VAR);
+    case 'w':
+      return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek())) advance();
+  return makeToken(identifierType());
+}
+
 void initScanner(const char* source) {
   scanner.start = source;
   scanner.current = source;
@@ -52,11 +179,14 @@ void initScanner(const char* source) {
 }
 
 Token scanToken() {
+  skipWhiteSpace();
   scanner.start = scanner.current;
 
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
   char c = advance();
+  if (isAlpha(c)) return identifier();
+  if (isDigit(c)) return number();
 
   switch (c) {
     case '(':
@@ -89,6 +219,8 @@ Token scanToken() {
       return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
     case '>':
       return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    case '"':
+      return string();
   }
   return errorToken("Unexpected character.");
 }
