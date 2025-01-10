dcde8db558fd872655ea995f33604f63f91f4904
Derek Fox <derekefox@gmail.com>
2025-01-10 14:38:59 -0500

    Finish adding support for comparison operators, and add missing disassembler code for OP_{NIL,TRUE,FALSE}

--- src/value.c
+++ src/value.c
@@ -4,6 +4,20 @@
 
 #include "memory.h"
 
+bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type) return false;
+  switch (a.type) {
+    case VAL_BOOL:
+      return AS_BOOL(a) == AS_BOOL(b);
+    case VAL_NIL:
+      return true;
+    case VAL_NUMBER:
+      return AS_NUMBER(a) == AS_NUMBER(b);
+    default:
+      return false;  // unreachable
+  }
+}
+
 void initValueArray(ValueArray* array) {
   array->capacity = 0;
   array->count = 0;
