bd9c50f988cdea0379a057650f8723c4552a2b0e
Derek Fox <derekefox@gmail.com>
2025-04-17 20:27:16 -0400

    Garbage collector coming along slowly

--- src/memory.h
+++ src/memory.h
@@ -7,6 +7,9 @@
 #define ALLOCATE(type, count) (type*)reallocate(NULL, 0, sizeof(type) * (count))
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void markObject(Obj* object);
+void markValue(Value value);
+void collectGarbage();
 void freeObjects();
 
 #define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
