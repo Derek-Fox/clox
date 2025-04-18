65967f1d153db92b6442024de9a53da951c9b9dc
Derek Fox <derekefox@gmail.com>
2025-04-18 11:19:28 -0400

    Garbage collector is done (?). Hard to test at the moment

--- src/vm.c
+++ src/vm.c
@@ -150,8 +150,8 @@ static bool isFalsey(Value value) {
 }
 
 static void concatenate() {
-  ObjString* b = AS_STRING(pop());
-  ObjString* a = AS_STRING(pop());
+  ObjString* b = AS_STRING(peek(0));
+  ObjString* a = AS_STRING(peek(1));
 
   int length = a->length + b->length;
   char* chars = ALLOCATE(char, length + 1);
@@ -160,11 +160,17 @@ static void concatenate() {
   chars[length] = '\0';
 
   ObjString* result = takeString(chars, length);
+  pop();
+  pop();
   push(OBJ_VAL(result));
 }
 
 void initVM() {
   resetStack();
+  
+  vm.bytesAllocated = 0;
+  vm.nextGC = 128; //1024 * 1024;
+
   vm.objects = NULL;
 
   vm.grayCount = 0;
